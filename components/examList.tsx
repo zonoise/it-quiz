e72c656c07a2723fa72aae39218dfc8b9b9ab1c1
zonoise <zonoise@gmail.com>
2021-08-02 02:22:29 +0900

    top page 見た目

--- components/examList.tsx
+++ components/examList.tsx
@@ -1,4 +1,4 @@
-import { Exam, Quiz } from '../types/types';
+import { Exam } from '../types/types';
 import Link from 'next/link';
 import React from 'react';
 
@@ -6,9 +6,9 @@ export const ExamListComponent: React.FC<{ exams: Exam[] }> = ({ exams }) => {
   return (
     <div>
       {exams.map((e) => (
-        <div key={e.id} className="flex">
+        <div key={e.id} className="flex shadow">
           <Link key={e.slug} href={`/quizzes/exams/${encodeURIComponent(e.slug)}`} passHref>
-            <a className="text-xl hover:bg-gray-100"> {e.title} </a>
+            <a className="text-lg w-full px-2 py-2 block hover:bg-gray-100"> {e.title} </a>
           </Link>
         </div>
       ))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,18 @@ import { Footer } from '../components/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-300">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <Navbar />
 
+      <div className="w-full p-3 flex justify-center">
+        <div className="w-full max-w-screen-md  p-3 shadow-md bg-yellow-100 rounded-xl">
+          <pre>{process.env.NEXT_PUBLIC_TOP_TEXT}</pre>
+        </div>
+      </div>
+
       <div className="flex flex-col items-center flex-grow ">
-        <div className="w-full max-w-screen-md bg-gray-400">
+        <div className="w-full max-w-screen-md bg-gray-50">
+          <h2 className="text-2xl  text-center font-bold py-3 bg-gray-200">試験一覧</h2>
           <ExamList />
         </div>
       </div>
